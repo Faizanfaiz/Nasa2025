296bdd39b8b31faac9be67eda962352d7808722b
amhawary <amin.hawary@gmail.com>
2025-10-05 21:26:59 +0100

    Changed to app.run()

--- v1.py
+++ v1.py
@@ -2110,7 +2110,7 @@ if SAM_VIEWER_AVAILABLE:
 
 
 if __name__ == "__main__":
-    app.run_server(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 
 
 
